3f751f9b27f4787afedb73e8727327bf5d6e18dc
TomMunro <thomas.munro@btinternet.com>
2019-06-22 17:31:55 +0100

    🐛If no input to TitleTextField return ""

--- src/components/PhotoPage/TitleTextField.js
+++ src/components/PhotoPage/TitleTextField.js
@@ -13,7 +13,8 @@ const styles = theme => ({
 });
 class TitleTextField extends Component {
 
-  dataFormater = value =>{
+  dataFormater = value => {
+    if (!value) return String(value)
     return this.props.field.type === enums.TYPES.number ? Number(value) : String(value);
   }
 
@@ -21,32 +22,32 @@ class TitleTextField extends Component {
     const valueFormated = this.dataFormater(e.target.value);
     const error = !e.target.value.match(this.props.field.regexValidation);
 
-    this.props.handleChange(valueFormated,error);
+    this.props.handleChange(valueFormated, error);
   }
 
   render() {
     // TODO: proptypes
     const { field, fieldValue, classes } = this.props;
     return (
-        <TextField
-          InputLabelProps= {{
-            shrink: true,
-            style: { color: '#000' },
-          }}
-          label={field.title}
-          fullWidth
-          id={'textfield' + field.title}
-          type={field.type}
-          required={true}
-          placeholder={field.placeholder}
-          className='text-field'
-          value={fieldValue.value}
-          onChange={this.onChangeHandler}
-          error= {fieldValue.error}
-          InputProps={Object.assign({
-            className: classes.cssUnderline
-          }, field.inputProps)}
-        />
+      <TextField
+        InputLabelProps={{
+          shrink: true,
+          style: { color: '#000' },
+        }}
+        label={field.title}
+        fullWidth
+        id={'textfield' + field.title}
+        type={field.type}
+        required={true}
+        placeholder={field.placeholder}
+        className='text-field'
+        value={fieldValue.value}
+        onChange={this.onChangeHandler}
+        error={fieldValue.error}
+        InputProps={Object.assign({
+          className: classes.cssUnderline
+        }, field.inputProps)}
+      />
     )
   }
 }
